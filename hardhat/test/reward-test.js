bf537000920562c8ce0511076041cce8122e29ec
0xrs <40622223+0xRS@users.noreply.github.com>
2021-03-08 14:38:13 +0530

    wip testing

--- hardhat/test/reward-test.js
+++ hardhat/test/reward-test.js
@@ -17,11 +17,13 @@ let owner
 let addr1
 let addr2
 let addrs
+const sleep = (delay) => new Promise((resolve)=>{console.log("Sleeping for "+delay + " secs...");setTimeout(resolve, delay*1000)});
 
 before(async() => {
 
     [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
-
+    console.log("===================Deploying Contracts=====================");
+    // console.log(addrs.map(addr=>addr.address));
     const CollateralERC20 = await ethers.getContractFactory("CollateralERC20");
     collateralERC20Contract = await CollateralERC20.deploy("Dai", "DAI");
     await collateralERC20Contract.deployed();
@@ -123,10 +125,14 @@ before(async() => {
     const ownerHaloBalance = await haloTokenContract.balanceOf(owner.address);
     await haloTokenContract.transfer(rewardsContract.address, ownerHaloBalance);
     console.log(ownerHaloBalance.toString() + " HALO tokens transfered to Rewards contract");
-    console.log();
+
+    const ownerUbeBalance = await ubeContract.balanceOf(owner.address);
+    await ubeContract.transfer(minterContract.address, ownerUbeBalance);
+    console.log(ownerUbeBalance.toString() + " UBE tokens transfered to Rewards contract");
+    console.log("==========================================================\n\n")
 })
 
-describe("1. Contract Deployments", function() {
+describe("Check Contract Deployments", function() {
     it("Collateral ERC20 should be deployed and owner should have initial mint", async() => {
         expect(await collateralERC20Contract.symbol()).to.equal("DAI");
         expect(await collateralERC20Contract.name()).to.equal("Dai");
@@ -159,10 +165,20 @@ describe("1. Contract Deployments", function() {
 
 })
 
-describe("2. When I deposit collateral tokens (DAI) on the Minter dApp, I start to earn HALO per block", function() {
-    it("I can deposit my collateral", async() => {
+describe("When I deposit collateral tokens (DAI) on the Minter dApp, I start to earn HALO per block", function() {
+    it("I can deposit DAI collateral to the minter", async() => {
+        const daiBalance = await collateralERC20Contract.balanceOf(owner.address);
+        await expect(minterContract.depositByCollateralAddress(
+        ethers.utils.parseEther('100'),
+        ethers.utils.parseEther('100'),
+        collateralERC20Contract.address)).to.not.be.reverted;
         // expect(await minterContract.depositByCollateralAddress())
     })
+    it("I can deposit DAI collateral to the minter", async() => {
+        // await sleep(5);
+
+    })
+
     // it("can redeem my collateral", async() => {
     //
     // })
@@ -172,7 +188,7 @@ describe("2. When I deposit collateral tokens (DAI) on the Minter dApp, I start 
 
 })
 
-describe("3. When I withdraw collateral tokens (DAI) on the Minter dApp, i stop earning HALO tokens", function() {
+describe("When I withdraw collateral tokens (DAI) on the Minter dApp, i stop earning HALO tokens", function() {
     // it("I can deposit my collateral", async() => {
     //     expect(await minterContract.depositByCollateralAddress())
     // })
@@ -185,28 +201,80 @@ describe("3. When I withdraw collateral tokens (DAI) on the Minter dApp, i stop 
 
 })
 
-describe("4. When I supply liquidity to an AMM, I am able to receive my proportion of HALO rewards per block", function() {
-    // it("I can deposit my collateral", async() => {
-    //     expect(await minterContract.depositByCollateralAddress())
-    // })
-    // it("can redeem my collateral", async() => {
-    //
-    // })
-    // it("can redeem my collateral", async() => {
-    //
-    // })
-
+describe("As an Admin, I can update AMM LP pool’s allocation points", function() {
+    it("AMM LP allocation points before", async() => {
+        expect((await rewardsContract.getAmmLpPoolInfo(lpTokenContract.address)).allocPoint.toString()).to.equal('10');
+    })
+    it("Total LP allocation points before", async() => {
+        expect(await rewardsContract.totalAmmLpAllocationPoints()).to.equal(10);
+    })
+    it("If caller is not contract owner, it should fail", async() => {
+        await expect(rewardsContract.connect(addr1).setAmmLpAlloc(lpTokenContract.address, 5)).to.be.revertedWith('Ownable: caller is not the owner');
+    })
+    it("If caller is contract owner, it should not fail; If AMM LP pool is whitelisted it should not fail; Set Amm LP pool allocs", async() => {
+        await expect(rewardsContract.connect(owner).setAmmLpAlloc(lpTokenContract.address, 5)).to.not.be.reverted;
+    })
+    it("AMM LP allocation points before", async() => {
+        expect((await rewardsContract.getAmmLpPoolInfo(lpTokenContract.address)).allocPoint.toString()).to.equal('5');
+    })
+    it("expectedAllocPoints = (totalAllocPoints - currentAllocPoints) + newAllocPoints = 10 - 10 + 5", async() => {
+        expect(await rewardsContract.totalAmmLpAllocationPoints()).to.equal(5);
+    })
 })
 
-describe("5. I can claim my unclaimed HALO tokens at any time on the Minter dApp", function() {
-    // it("I can deposit my collateral", async() => {
-    //     expect(await minterContract.depositByCollateralAddress())
-    // })
-    // it("can redeem my collateral", async() => {
-    //
-    // })
-    // it("can redeem my collateral", async() => {
-    //
-    // })
+describe("As an Admin, I can update minter lp collateral allocation points", function() {
+    it("DAI allocation points before", async() => {
+        expect((await rewardsContract.getMinterLpPoolInfo(collateralERC20Contract.address)).allocPoint.toString()).to.equal('10');
+    })
+    it("Total Minter LP allocation points before", async() => {
+        expect(await rewardsContract.totalMinterLpAllocationPoints()).to.equal(10);
+    })
+    it("If caller is not contract owner, it should fail", async() => {
+        await expect(rewardsContract.connect(addr1).setMinterLpAlloc(collateralERC20Contract.address, 5)).to.be.revertedWith('Ownable: caller is not the owner');
+    })
+    it("If caller is contract owner, it should not fail; If collateral type is whitelisted it should not fail; Set Minter Lp pool allocs", async() => {
+        await expect(rewardsContract.connect(owner).setMinterLpAlloc(collateralERC20Contract.address, 5)).to.not.be.reverted;
+    })
+    it("DAI LP allocation points before", async() => {
+        expect((await rewardsContract.getMinterLpPoolInfo(collateralERC20Contract.address)).allocPoint.toString()).to.equal('5');
+    })
+    it("expectedAllocPoints = (totalAllocPoints - currentAllocPoints) + newAllocPoints = 10 - 10 + 5", async() => {
+        expect(await rewardsContract.totalMinterLpAllocationPoints()).to.equal(5);
+    })
+})
 
+describe("As an Admin, I can remove whitelisted AMM LP pool", function() {
+    it("Should be valid amm lp", async() => {
+        expect(await rewardsContract.isValidAmmLp(lpTokenContract.address)).to.equal(true);
+    })
+    it("If caller is not contract owner, it should fail", async() => {
+        await expect(rewardsContract.connect(addr1).removeAmmLp(lpTokenContract.address)).to.be.revertedWith('Ownable: caller is not the owner');
+    })
+    it("If caller is contract owner, it should not fail; If AMM LP pool is whitelisted it should not fail; Remove AMM LP pool from ammLpPools", async() => {
+        await expect(rewardsContract.connect(owner).removeAmmLp(lpTokenContract.address)).to.not.be.reverted;
+    })
+    it("If AMM LP pool is not whitelisted is should fail", async() => {
+        await expect(rewardsContract.connect(owner).removeAmmLp(lpTokenContract.address)).to.be.revertedWith('AMM LP Pool not whitelisted');
+    })
+    it("Should not be valid amm lp", async() => {
+        expect(await rewardsContract.isValidAmmLp(lpTokenContract.address)).to.equal(false);
+    })
+})
+
+describe("As an Admin, I can remove whitelisted collateral type", function() {
+    it("Should be valid collateral type", async() => {
+        expect(await rewardsContract.isValidMinterLp(collateralERC20Contract.address)).to.equal(true);
+    })
+    it("If caller is not contract owner, it should fail", async() => {
+        await expect(rewardsContract.connect(addr1).removeMinterCollateralType(collateralERC20Contract.address)).to.be.revertedWith('Ownable: caller is not the owner');
+    })
+    it("If caller is contract owner, it should not fail; If Minter collateral type is whitelisted it should not fail; Remove Minter collateral type from minterLpPools", async() => {
+        await expect(rewardsContract.connect(owner).removeMinterCollateralType(collateralERC20Contract.address)).to.not.be.reverted;
+    })
+    it("If collateral type is not whitelisted is should fail", async() => {
+        await expect(rewardsContract.connect(owner).removeMinterCollateralType(collateralERC20Contract.address)).to.be.revertedWith('Collateral type not whitelisted');
+    })
+    it("Should not be valid collateral type", async() => {
+        expect(await rewardsContract.isValidMinterLp(collateralERC20Contract.address)).to.equal(false);
+    })
 })
