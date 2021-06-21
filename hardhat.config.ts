3f37a82042ab64a7e106784c7849a7fbe6dabc7d
Chris <ctverceles@gmail.com>
2021-06-21 13:30:59 +0800

    remove task from hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,11 @@
 require('dotenv').config()
 
 import { task } from 'hardhat/config'
-import { HardhatUserConfig } from 'hardhat/types'
 import '@nomiclabs/hardhat-waffle'
 import 'hardhat-gas-reporter'
 import '@nomiclabs/hardhat-ethers'
 import 'solidity-coverage'
 import '@nomiclabs/hardhat-etherscan'
-import sleep from './scripts/util/sleep'
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,26 +16,6 @@ task('accounts', 'Prints the list of accounts', async (args, hre) => {
     console.log(account.address)
   }
 })
-
-/**
- * use this if you ran the deploy script, but timed out on terminal while waiting for tx confirmation
- * sample: npx hardhat verifyHalo --address '' --name 'Rainbow Token' --symbol 'RNBW' --network "mainnet"
- */
-task('verifyHalo', 'verifies predeployed Halo token')
-  .addParam(
-    'address',
-    'The mainnet address of the deployed contract to be verified'
-  )
-  .addParam('name', 'name of the deployed token')
-  .addParam('symbol', 'symbol of the deployed token')
-  .setAction(async (args, hre) => {
-    // auto verify halo token
-    console.log('verifying haloToken')
-    await hre.run('verify:verify', {
-      address: args.address,
-      constructorArguments: [args.name, args.symbol]
-    })
-  })
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
