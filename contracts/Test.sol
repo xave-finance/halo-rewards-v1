26ed8ac9a9dba4e4ca43dda6b2fd810309172612
0xrs <40622223+0xrs@users.noreply.github.com>
2021-06-22 22:15:25 +0530

    fix tests

--- contracts/Test.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity ^0.6.12;
-import {SafeMath} from '@openzeppelin/contracts/math/SafeMath.sol';
-contract Test {
-    using SafeMath for uint256;
-    function get() public view returns (uint256) {
-        return uint256(250000*10**18).mul(8000).div(10000).div(2592000);
-    }
-}
