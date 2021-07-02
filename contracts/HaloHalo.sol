7081c74528dd3c18ef29649eef5c03e9f07451ff
0xrs <40622223+0xrs@users.noreply.github.com>
2021-07-02 11:16:32 +0530

    fixes issue 1

--- contracts/HaloHalo.sol
+++ contracts/HaloHalo.sol
@@ -8,7 +8,7 @@ import {SafeMath} from '@openzeppelin/contracts/math/SafeMath.sol';
 
 contract HaloHalo is ERC20('Rainbow Pool', 'xRNBW') {
   using SafeMath for uint256;
-  IERC20 public halo;
+  IERC20 immutable halo;
   uint256 public constant DECIMALS = 1e18;
   uint256 public genesisTimestamp;
 
